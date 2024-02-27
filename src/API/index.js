6c8bfcbc240b85f9f10c33aefddf205dac28aca9
HxClover <rbecca.lam@gmail.com>
2024-02-26 22:07:28 -0800

    update

---
+++ src/API/index.js
@@ -0,0 +1,13 @@
+//Write a function to fetch all the players from the API
+//Then, import into AllPlayers.jsx component
+
+export async function fetchPlayers() {
+    try {
+        const response = await fetch('');
+        const data = await response.json();
+
+    }
+}
+
+
+//Write a funtion to fetch single player in API and render (just like above)
